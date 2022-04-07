717f7219c1c2978dfd759538204e809509daec7a
JosephJoeyJoe <88215210+JosephJoeyJoe@users.noreply.github.com>
2022-04-06 20:40:25 -0400

    added server connection and schema

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const controllers = require('./controllers');
+const sequelize = require("./config/connection");
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const path = require('path');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'secret',
+    cookie: {
+        maxAge: null},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(controllers);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+});
